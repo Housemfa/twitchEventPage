77dd661c66fe50cb64fbb69d5ae916bf36c49725
Chae <bubsakk@gmail.com>
2020-08-02 16:36:39 +0900

    auto Commit!

--- chat.js
+++ chat.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
-var http = require('http').Server(app); //1
-var io = require('socket.io')(http);    //1
+var http = require('http').Server(app); 
+var io = require('socket.io')(http);    
 var id = "";
 var SEC_VALUE = "";
 var admin = "";
+var M_VALUE="";//masked answer
 app.get('/', function (req, res) {  //2
+	console.log(req.query);
+	id = req.query.id;
+	res.sendFile(__dirname + '/client.html');
+});
+app.get('/log', function (req, res) {  //2
 	console.log(req.query);
 	id = req.query.id;
 	res.sendFile(__dirname + '/client.html');
@@ -19,25 +25,31 @@ io.on('connection', function (socket) { //3
 	if (name != null) { name = id; }              //3-1
 	else { name = "Unknown Dodie" }
 	io.to(socket.id).emit('change name', name);   //3-1
-
+	
+	if(M_VALUE!=""){
+		console.log('newbie!'+M_VALUE+"<<")
+		io.emit('maskedAns', M_VALUE);
+	}
 	socket.on('disconnect', function () { //3-2
 		console.log('user disconnected: ', socket.id);
+	});
+	socket.on('refresh', function () { //3-2
+		console.log('사용자 전체 REFRESH');
+		socket.broadcast.emit('refresh');
 	});
 	socket.on('setans', function (value, mValue) { //3-2
 		if (admin == "") {
 			admin = socket.id
 		}
-		console.log(admin + " / " + socket.id)
-		console.log('setans: ', value);
+		console.log('정답설정: ', value);
 		SEC_VALUE = value;
+		M_VALUE = mValue;
 		io.emit('maskedAns', mValue);
 		socket.broadcast.emit('maskedAns', mValue);
 	});
 
 	socket.on('send message', function (name, text) { //3-3
 		if (text.substr(0, 1) == '@') {
-			//reserved area
-			console.log("@@@");
 			var args = text.split(' ');
 			var newName = args[1];
 			console.log(name + " -> " + newName);
@@ -56,10 +68,9 @@ io.on('connection', function (socket) { //3
 });
 var server_port = process.env.YOUR_PORT || process.env.PORT || 8000;
 
-//http.listen(process.env.port || 8000, function(){ //4
 http.listen(server_port, function () { //4
 	console.log('server on! listening ' + server_port);
-	//  console.log('server on! listening '+process.env.port+' || 8000');
+
 });
 
 function saveData(Account, msg) {
